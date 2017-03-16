ee3997e598fddb781037f924ac413da98643693e
Andres Recalde <andres.recalde@lacolombe.net>
2017-03-16 11:10:04 -0400

    fixed all typos and typing

--- models/tables/shipments_items.sql
+++ models/tables/shipments_items.sql
@@ -5,11 +5,11 @@ s.order_number,
 s.shopify_order_number,
 sku, 
 quantity,
-weight_oz,
-weight_lbs,
+si.weight_oz,
+si.weight_lbs,
 (shipping_cost / total_quantity) * quantity as line_item_shipping_cost
 from {{ref('webhook_shipments_items')}} si
-join {{ref('webhook_shipments')}} s on s.order_id = si.order_id
+join {{ref('shipments')}} s on s.order_id = si.order_id
 
 UNION
 
@@ -20,8 +20,8 @@ si.order_number,
 si.shopify_order_number,
 sku, 
 quantity,
-weight_oz,
-weight_lbs,
+si.weight_oz,
+si.weight_lbs,
 (shipping_cost / total_quantity) * quantity as line_item_shipping_cost
 from {{ref('file_shipments_items')}} si
-join {{ref('file_shipments')}} s on s.order_number = si.order_number
+join {{ref('shipments')}} s on s.order_number = si.order_number
