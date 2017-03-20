27c769ba4171d38d02ced9a696607659938ea390
Andres Recalde <andres.recalde@lacolombe.net>
2017-03-20 00:14:26 -0400

    fixed typos

--- models/base/webhook_shipments_items.sql
+++ models/base/webhook_shipments_items.sql
@@ -4,5 +4,5 @@ _sdc_source_key_orderid as order_id,
 upper(sku) as sku, 
 quantity,
 weight__value as weight_oz,
-round((weight__value::decimal(16,2) / 16,1) as weight_lbs
+round(weight__value::decimal(16,2) / 16,1) as weight_lbs
 from shipstation.webhook_shipments__shipmentitems 
