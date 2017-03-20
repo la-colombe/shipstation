6a115ceed47e8b07d99b8db530c9b03aecf6372e
Andres Recalde <andres.recalde@lacolombe.net>
2017-03-20 00:15:36 -0400

    fixed typos

--- models/tables/shopify_shipments_items_aggregate.sql
+++ models/tables/shopify_shipments_items_aggregate.sql
@@ -1,6 +1,6 @@
 select
-shopify_order_number,
-count(shopify_order_number) as shipments,
+shopify_line_item_id,
+count(shopify_line_item_id) as shipments,
 sum(line_item_shipping_cost) as line_item_shipping_cost,
 sum(weight_lbs) as total_weight_lbs
 from {{ref('shipments_items')}}
