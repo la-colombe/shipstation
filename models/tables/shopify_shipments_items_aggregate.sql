bf68e976e89b9c845c1c1da9fc823f272ac3d3d6
Andres Recalde <andres.recalde@lacolombe.net>
2017-06-08 18:24:15 -0400

    added carrier and service to items

--- models/tables/shopify_shipments_items_aggregate.sql
+++ models/tables/shopify_shipments_items_aggregate.sql
@@ -2,6 +2,8 @@ select
 shopify_line_item_id,
 count(shopify_line_item_id) as shipments,
 sum(line_item_shipping_cost) as line_item_shipping_cost,
-sum(weight_lbs) as total_weight_lbs
+sum(weight_lbs) as total_weight_lbs,
+max(carrier) as carrier,
+max(service) as service
 from {{ref('shipments_items')}}
 group by 1
