daeafeb6ecc149c6dc8f2397080b4b14938bb37f
Andres Recalde <andres.recalde@lacolombe.net>
2018-12-25 11:42:04 -0500

    cleaned up

--- models/aggregates/shopify_shipments_aggregate.sql
+++ models/aggregates/shopify_shipments_aggregate.sql
@@ -1,7 +1,6 @@
 select
 shopify_order_number,
 count(shopify_order_number) as shipments,
-sum(shipping_cost) as shipping_cost,
-sum(total_weight_lbs) as total_weight_lbs
+sum(shipping_cost) as shipping_cost
 from {{ref('shipments')}}
 group by 1
--- models/aggregates/shopify_shipments_items_aggregate.sql
+++ models/aggregates/shopify_shipments_items_aggregate.sql
@@ -1,8 +1,6 @@
 select
 shopify_line_item_id,
-count(shopify_line_item_id) as shipments,
 sum(line_item_shipping_cost) as line_item_shipping_cost,
-sum(weight_lbs) as total_weight_lbs,
 max(carrier) as carrier,
 max(service) as service,
 min(ship_date) as first_ship_date,
