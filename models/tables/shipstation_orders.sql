c2e5dc773870189c7ca71938ef6819ca8d747551
Andres Recalde <andres.recalde@lacolombe.net>
2019-01-07 11:07:20 -0500

    added shipstation orders

---
+++ models/tables/shipstation_orders.sql
@@ -0,0 +1,31 @@
+{{
+config({
+    "materialized" : "table",
+    "sort" : "modified_at",
+    "post-hook" : [
+      "grant select on table {{this}} to group non_gl_read_only"
+      ]
+    })
+}}
+
+select
+  --IDs
+  orderkey as order_key,
+  orderid as order_id,
+  ordernumber as order_number,
+  '1' || split_part(ordernumber,'-',1) as shopify_order_number,
+  advancedoptions__customfield3 as shopify_fulfillment_id,
+  case advancedoptions__warehouseid
+    when 14561 then 'Tioga'
+    when 127295 then 'Michigan'
+    when 139016 then 'RLS Warehouse'
+    when 163472 then 'Frogtown'
+    else advancedoptions__warehouseid::char(8)
+  end as ship_from_warehouse,
+  --Status
+  orderstatus as order_status,
+  shipto__addressverified as address_verification_status,
+  --Timestamps
+  createdate::timestamp as created_at,
+  modifydate::timestamp as modified_at
+from shipstation.orders
