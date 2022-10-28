9f648e7b6dbbbbbf6575676de2692996e2434e8e
chris-schmid <chris.schmid@lacolombe.net>
2022-10-28 18:08:44 -0400

    Unioned new meltano shipments tables

--- models/base/shipstation_shipment_items.sql
+++ models/base/shipstation_shipment_items.sql
@@ -1,8 +1,26 @@
+--Old Tap
+
 select
-lineitemkey as shopify_line_item_id,
-_sdc_source_key_shipmentid as shipment_id,
-upper(sku) as sku,
-quantity,
-weight__value::decimal(16,2) as weight_oz,
-round(weight__value::decimal(16,2) / 16,1) as weight_lbs
-from shipstation.shipments__shipmentitems
+	si.lineitemkey as shopify_line_item_id,
+	si._sdc_source_key_shipmentid as shipment_id,
+	upper(si.sku) as sku,
+	si.quantity,
+	si.weight__value::decimal(16,2) as weight_oz,
+	round(si.weight__value::decimal(16,2) / 16,1) as weight_lbs
+from shipstation.shipments__shipmentitems si
+join shipstation.shipments s on si._sdc_source_key_shipmentid = s.shipmentid
+where s.createdate::date < '2022-09-20'
+
+union all
+
+--Meltano Tap
+select
+	si.lineitemkey as shopify_line_item_id,
+	si._sdc_source_key_shipmentid as shipment_id,
+	upper(si.sku) as sku,
+	si.quantity,
+	si.weight__value::decimal(16,2) as weight_oz,
+	round(si.weight__value::decimal(16,2) / 16,1) as weight_lbs
+from shipstation.shipments__shipmentitems si
+join shipstation.shipments s on si._sdc_source_key_shipmentid = s.shipmentid
+where s.createdate::date >= '2022-09-20'
