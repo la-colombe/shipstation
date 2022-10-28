bf946983cb2629926ef5c11e0b6abdb47028feaa
chris-schmid <chris.schmid@lacolombe.net>
2022-10-28 18:19:00 -0400

    Added orders union

--- models/tables/shipstation_orders.sql
+++ models/tables/shipstation_orders.sql
@@ -8,6 +8,8 @@ config({
     })
 }}
 
+--Old tap
+
 select
   --IDs
   orderkey as order_key,
@@ -29,3 +31,31 @@ select
   createdate::timestamp as created_at,
   modifydate::timestamp as modified_at
 from shipstation.orders
+where createdate::date < '2022-09-20'
+
+union all 
+
+--Meltano tap
+
+select
+  --IDs
+  orderkey as order_key,
+  orderid as order_id,
+  ordernumber as order_number,
+  '1' || split_part(ordernumber,'-',1) as shopify_order_number,
+  advancedoptions__customfield3 as shopify_fulfillment_id,
+  case advancedoptions__warehouseid
+    when 14561 then 'Tioga'
+    when 127295 then 'Michigan'
+    when 139016 then 'RLS Warehouse'
+    when 163472 then 'Frogtown'
+    else advancedoptions__warehouseid::char(8)
+  end as ship_from_warehouse,
+  --Status
+  orderstatus as order_status,
+  shipto__addressverified as address_verification_status,
+  --Timestamps
+  createdate::timestamp as created_at,
+  modifydate::timestamp as modified_at
+from meltano_shipstation.orders
+where createdate::date >= '2022-09-20'
